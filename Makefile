37658ccb5c1fbdda80be9e36577ddb4d7b1d2b1e
magnus <mingran@live.com>
2016-06-01 08:29:20 +0800

    commit project

---
+++ Makefile
@@ -0,0 +1,35 @@
+C=gcc
+CXX=g++ -std=c++11
+CFLAGS=-I./include
+LDFLAGS=-Wl,--no-as-needed -ldl -pthread
+LINK=g++
+
+SRC=./src
+INCLUDE=./include
+BUILD=./build
+TOOL_SRC=./tool
+
+.PHONY: all
+all: $(BUILD)/main
+
+$(BUILD)/main: $(BUILD)/main.o $(BUILD)/combinition.o $(BUILD)/libClassifier.o $(BUILD)/sqlite3.o $(BUILD)/TC_process.o
+	$(LINK) $(LDFLAGS) $^ -o $@
+
+$(BUILD)/combinition.o: $(SRC)/combinition.cpp
+	$(CXX) $^ $(CFLAGS) -c -o $@
+
+$(BUILD)/libClassifier.o: $(SRC)/libClassifier.cpp
+	$(CXX) $^ $(CFLAGS) -c -o $@
+
+$(BUILD)/sqlite3.o: $(SRC)/sqlite3.c
+	$(C) $^ $(CFLAGS) -c -o $@
+
+$(BUILD)/TC_process.o: $(SRC)/TC_process.cpp
+	$(CXX) $^ $(CFLAGS) -c -o $@
+
+$(BUILD)/main.o: $(TOOL_SRC)/main.cpp
+	$(CXX) $^ $(CFLAGS) -c -o $@
+
+clean:
+	rm build/*.o
+	rm -f build/main
---
+++ include/TC_process.h
@@ -0,0 +1,58 @@
+// #include "stdafx.h"
+
+#include <iostream>
+#include <fstream>
+//#include "../cppjieba/headers.h"
+#include "combinition.h"
+
+#include <vector>
+#include <list>
+#include <string.h>
+
+#include "sqlite3.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "libClassifier.h"
+
+
+#define N_WORD 10
+#define N_DIMEN 5
+
+#pragma once
+
+
+extern std::vector<int> v_query;
+
+
+////
+void cut(const CppJieba::SegmentInterface * seg, std::list<char *> & l_lines, std::vector<std::vector<char *> *> & v_lines_words, int n_word);
+
+void cut(const CppJieba::SegmentInterface * seg, const char * const filePath, char * outfile, int n_word);
+
+void query_word(sqlite3 * conn, char * word);
+
+int sqlite3_exec_callback(void *data, int n_columns, char **col_values, char **col_names);
+
+
+//�ļ���������
+class termFilter
+{
+private:
+	std::vector<char *> cantPassediterms;
+
+public:
+	termFilter( const char * loadPath );
+	~termFilter();
+
+	void appendFilter( char * loadPath );
+
+	bool termIsPass(char *);
+};
+
+
+
+void cut(const CppJieba::SegmentInterface * seg, char ** p_text, int n_text, std::vector<std::vector<char *> *> & v_lines_words, int n_word, termFilter & filter);
+
+void textCategorization_new(char ** p_text, int n_text, int * p_labels, char * outputPath=NULL);
